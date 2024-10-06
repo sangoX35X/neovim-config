7557a0b01b3f356b5b8dab85be311b5cbf28713c
sango_35 <sango_35_pro@hotmail.com>
2024-10-07 01:42:02 +0900

    change: cahnge a setting of toggleterm.nvim
    change terminal_mappings false.

--- init.lua
+++ init.lua
@@ -796,7 +796,8 @@ local plugins = {
 		version = "*",
 		opts = {
 			open_mapping = "<Leader>tt",
-			insert_mappings = false
+			insert_mappings = false,
+			terminal_mappings = false
 		}
 	},
 	{
