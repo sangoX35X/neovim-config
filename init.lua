cadea93ed14e147f4d1c55f6a27fe003d52d333e
sango_35 <sango_35_pro@hotmail.com>
2024-11-25 22:56:32 +0900

    add: add a plugin `nvim-hlslens`

--- init.lua
+++ init.lua
@@ -1010,6 +1010,18 @@ local plugins = {
 		},
 		config = true
 	},
+	{
+		"kevinhwang91/nvim-hlslens",
+		keys = {
+			{ "n", [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+			{ "N", [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+			{ "*", [[*<Cmd>lua require('hlslens').start()<CR>]] },
+			{ "#", [[#<Cmd>lua require('hlslens').start()<CR>]] },
+			{ "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]] },
+			{ "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]] }
+		},
+		config = true
+	},
 --##plugins-nvimTree
 	{
 		"nvim-tree/nvim-tree.lua",
